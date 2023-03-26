eeef3ec0a3f5b2481be08dff6c0ebd0a21e47e89
vnneji <91854619+vnneji@users.noreply.github.com>
2023-03-26 21:08:23 +0100

    Update streamlit_app.py
    added fruit adviser

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title('My Mom''s New Healthy Diner')
 
@@ -25,6 +24,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 #streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # write your own comment -what does the next line do? Normalize semi-structured JSON data into a flat table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
